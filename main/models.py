59ef297b1fae660ffc3899ad8bf662b49f27cd7f
Rakhmatullo <qosimovrahmatullo006@outlook.com>
2023-05-16 23:26:11 +0500

    add models.py #2

--- main/models.py
+++ main/models.py
@@ -1,5 +1,62 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class BasicUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    city = models.CharField(max_length=100)
+    school = models.CharField(max_length=100)
+    more_details = models.TextField()
 
 
+class QuestionType(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Option(models.Model):
+    text = models.CharField(max_length=100)
+    question = models.ForeignKey('Question', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
+
+
+class Question(models.Model):
+    text = models.TextField()
+    question_type = models.ForeignKey(QuestionType, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
+
+    def add_option(self, text, is_correct=False):
+        option = Option.objects.create(
+            text=text, question=self, is_correct=is_correct)
+        return option
+
+
+class Exam(models.Model):
+    name = models.CharField(max_length=100)
+    questions = models.ManyToManyField(Question)
+
+    def __str__(self):
+        return self.name
+
+
+class QuestionGroup(models.Model):
+    name = models.CharField(max_length=100)
+    questions = models.ManyToManyField(Question)
+
+    def __str__(self):
+        return self.name
+
+
+class Results(models.Model):
+    user = models.ForeignKey(BasicUser, on_delete=models.CASCADE)
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
+    score = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.user.user.username} - {self.exam.name}"

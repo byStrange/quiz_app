fe7808c26638e8bc582f9605f49f104333bbddf3
Rakhmatullo <qosimovrahmatullo006@outlook.com>
2023-05-16 23:44:24 +0500

    test template #4

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "main"
 
 urlpatterns = [
+    path("test/", views.test, name="test")
 ]
--- quiz_app/settings.py
+++ quiz_app/settings.py
@@ -105,7 +105,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_FILES_DIRS = [
+STATICFILES_DIRS = [
     BASE_DIR / 'staticfiles'
 ]
 

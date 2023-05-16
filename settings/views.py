ab59c988711725549e805cae7f1b1660b59e44e7
Rakhmatullo <qosimovrahmatullo006@outlook.com>
2023-05-17 01:28:49 +0500

    +

--- settings/views.py
+++ settings/views.py
@@ -15,8 +15,9 @@ def exams(request):
     context = {"exams": exams}
     return render(request, "settings/exams.html", context)
 
+
 @staff_member_required
 def users(request):
     users = BasicUser.objects.all()
-    context = {"users", users}
+    context = {"users": users}
     return render(request, "settings/users.html", context)

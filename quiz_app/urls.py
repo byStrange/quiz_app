6a9605edb0d9877b99c5057df3416e9c73ff655c
Rakhmatullo <qosimovrahmatullo006@outlook.com>
2023-05-16 23:35:55 +0500

    add assets and few changes #3

--- quiz_app/urls.py
+++ quiz_app/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("main.urls", namespace="main")),
+    path("main/", include("main.urls", namespace="main")),
     path('admin/', admin.site.urls),
 ]
